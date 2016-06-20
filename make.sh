09cb7a04e32cf085f73e5ea272c133309a670987
calin tenitchi <calin.tenitchi@gmail.com>
2016-06-21 00:45:28 +0700

    .

--- make.sh
+++ make.sh
@@ -1,12 +1,21 @@
-OPT=-O3
-clang++ -std=c++14 $OPT -o compi src/main.cpp &&
+#for o in s 0 1 2 3;do
+for o in 3;do
+#  echo " * optimiziation option: $o "
+  clang++ -std=c++14 -O$o -S src/main.cpp &&
+  echo -n 'callqs: ' 
+  cat main.s|grep callq|sort -u|wc|awk '{print $1}' &&
+  clang++ -std=c++14 -O$o -o compi src/main.cpp &&
+  ls --color -la compi
+done
 ./compi > gen.s &&
 nasm -f elf64 gen.s && 
 ld -s -o binary gen.o &&
-echo && 
-ls --color -la compi &&
+#ls --color -la compi &&
 ls --color -la gen.s &&
 ls --color -la binary &&
-echo && 
+echo &&
+echo -n '     src: ' && cat src/*|wc &&
+echo -n ' gzipped: ' && cat src/*|gzip|wc && 
+echo &&
 ./binary
 
