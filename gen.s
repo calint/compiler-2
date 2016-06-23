922863e1ab740b3f68c02777bb78952e728d9283
calin tenitchi <calin.tenitchi@gmail.com>
2016-06-23 22:26:24 +0700

    .

--- gen.s
+++ gen.s
@@ -4,72 +4,72 @@ _start:
   mov ebp,stk
   mov esp,stkend
 ; main(){  [0]
-;  var b=
-;  var c=
-   mov dword[ebp+4],2
-;  b=
-   mov dword[ebp+0],3
-;  c=
-   mov dword[ebp+4],4
-   _loop_674:
-     _loop_684:
-;      print(prompt.len prompt){  [695]
-         mov ecx,prompt
-         mov edx,prompt.len
-         mov ebx,1
-         mov eax,4
-         int 0x80
-       _end_print_695:
-;      var ln=
-;       read(name.len name):ln{  [730]
-          mov esi,name
-          mov edx,name.len
-          xor eax,eax
-          xor edi,edi
-          syscall
-          mov dword[ebp+8],eax
-        _end_read_730:
-;      if [753]
-;      orl(eq eq):eax{  [756]
-;      eq(ln 1):ebx{  [760]
-         cmp dword[ebp+8],1
-         cmove ebx,[mem1]
-         cmovne ebx,[mem0]
-       _end_eq_760:
-;      eq(ln 2):ecx{  [769]
-         cmp dword[ebp+8],2
-         cmove ecx,[mem1]
-         cmovne ecx,[mem0]
-       _end_eq_769:
-         mov eax,ebx
-         or eax,ecx
-       _end_orl_756:
-       cmp eax,1
-       jne _end__if_753
-         jmp _loop_684
-       _end__if_753:
-;      print(hello.len hello){  [804]
-         mov ecx,hello
-         mov edx,hello.len
-         mov ebx,1
-         mov eax,4
-         int 0x80
-       _end_print_804:
-;      print(ln name){  [830]
-         mov ecx,name
-         mov edx,dword[ebp+8]
-         mov ebx,1
-         mov eax,4
-         int 0x80
-       _end_print_830:
-     jmp _loop_684
-     _end_loop_684:
-   jmp _loop_674
-   _end_loop_674:
-;  exit(){  [853]
+   _loop_648:
+;    print(prompt.len prompt){  [658]
+       mov ecx,prompt
+       mov edx,prompt.len
+       mov ebx,1
+       mov eax,4
+       int 0x80
+     _end_print_658:
+;    var ln=
+;     read(name.len name):ln{  [692]
+        mov esi,name
+        mov edx,name.len
+        xor eax,eax
+        xor edi,edi
+        syscall
+        mov dword[ebp+0],eax
+      _end_read_692:
+;    if [714]
+;    orl(eq eq):eax{  [717]
+;    eq(ln 1):ebx{  [721]
+       cmp dword[ebp+0],1
+       cmove ebx,[mem1]
+       cmovne ebx,[mem0]
+     _end_eq_721:
+;    eq(ln 2):ecx{  [730]
+       cmp dword[ebp+0],2
+       cmove ecx,[mem1]
+       cmovne ecx,[mem0]
+     _end_eq_730:
+       mov eax,ebx
+       or eax,ecx
+     _end_orl_717:
+     cmp eax,1
+     jne _end__if_714
+       jmp _loop_648
+     _end__if_714:
+;    if [762]
+;    eq(ln 3):ecx{  [765]
+       cmp dword[ebp+0],3
+       cmove ecx,[mem1]
+       cmovne ecx,[mem0]
+     _end_eq_765:
+     cmp ecx,1
+     jne _end__if_762
+       jmp _end_loop_648
+     _end__if_762:
+;    print(hello.len hello){  [793]
+       mov ecx,hello
+       mov edx,hello.len
+       mov ebx,1
+       mov eax,4
+       int 0x80
+     _end_print_793:
+;    print(ln name){  [818]
+       mov ecx,name
+       mov edx,dword[ebp+0]
+       mov ebx,1
+       mov eax,4
+       int 0x80
+     _end_print_818:
+   jmp _loop_648
+   _end_loop_648:
+;  exit(){  [837]
      mov eax,1
      int 0x80
-   _end_exit_853:
+   _end_exit_837:
 ; }
 
 
