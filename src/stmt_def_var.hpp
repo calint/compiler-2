6f50e96f591ca9d11bf1be8ce9318c68af152a9c
calin tenitchi <calin.tenitchi@gmail.com>
2016-06-26 02:38:48 +0700

    .

--- src/stmt_def_var.hpp
+++ src/stmt_def_var.hpp
@@ -14,43 +14,43 @@
 class stmt_def_var final:public statement{public:
 
 	inline stmt_def_var(const statement&parent,const token&tkn,tokenizer&t):
-		statement{parent,tkn}
+		statement{parent,tkn},
+		ident_{t.next_token()}
 	{
-		identifier=t.next_token();
 		if(!t.is_next_char('='))
 			return;
-		initial_value=create_statement_from_tokenizer(*this,t);
+		initial_expression_=create_statement_from_tokenizer(*this,t);
 	}
 
 	inline void source_to(ostream&os)const override{
 		statement::source_to(os);
-		identifier.source_to(os);
-		if(initial_value){
+		ident_.source_to(os);
+		if(initial_expression_){
 			os<<"=";
-			return initial_value->source_to(os);
+			return initial_expression_->source_to(os);
 		}
 	}
 
 	inline void compile(toc&tc,ostream&os,size_t indent_level)const override{
 		indent(os,indent_level,true);
-		os<<"var "<<identifier.name()<<"="<<endl;
+		os<<"var "<<ident_.name()<<"="<<endl;
 
-		tc.framestk().add_var(identifier.name(),"");
+		tc.framestk().add_var(ident_.name(),"");
 
-		if(initial_value){
-			if(initial_value->is_expression()){
-				initial_value->set_expression_dest_nasm_identifier(identifier.name());
-				initial_value->compile(tc,os,indent_level+1);
+		if(initial_expression_){
+			if(initial_expression_->is_expression()){
+				initial_expression_->set_expression_dest_nasm_identifier(ident_.name());
+				initial_expression_->compile(tc,os,indent_level+1);
 			}else{
 				indent(os,indent_level,false);
-				const string&ra=tc.resolve_ident_to_nasm(*this,identifier.name());
-				const string&rb=tc.resolve_ident_to_nasm(*initial_value,initial_value->tok().name());
+				const string&ra=tc.resolve_ident_to_nasm(*this,ident_.name());
+				const string&rb=tc.resolve_ident_to_nasm(*initial_expression_,initial_expression_->tok().name());
 				os<<"mov "<<ra<<","<<rb<<endl;
 			}
 		}
 	}
 
 private:
-	token identifier;
-	up_statement initial_value;
+	token ident_;
+	up_statement initial_expression_;
 };
--- src/token.hpp
+++ src/token.hpp
@@ -12,11 +12,11 @@ using namespace std;
 class token final{public:
 
 	inline token(string wb,size_t n,string tk,size_t n2,string wa):
-		ws_before{wb},
-		token_start_char_{n},
+		ws_left_{wb},
+		start_char_{n},
 		name_{tk},
-		token_end_char_{n2},
-		ws_after{wa}
+		end_char_{n2},
+		ws_right_{wa}
 	{}
 
 	inline token(const string&name):
@@ -27,32 +27,34 @@ class token final{public:
 		token{"",0,"",0,""}
 	{}
 
+	// token{othertoken}
 	inline token(const token&tk):
-		ws_before{tk.ws_before},
-		token_start_char_{tk.token_start_char_},
+		ws_left_{tk.ws_left_},
+		start_char_{tk.start_char_},
 		name_{tk.name_},
-		token_end_char_{tk.token_end_char_},
-		ws_after{tk.ws_after}
+		end_char_{tk.end_char_},
+		ws_right_{tk.ws_right_}
 	{}
 
+	// return move(token{})
 	inline token(const token&&tk):
-		ws_before{move(tk.ws_before)},
-		token_start_char_{tk.token_start_char_},
+		ws_left_{move(tk.ws_left_)},
+		start_char_{tk.start_char_},
 		name_{move(tk.name_)},
-		token_end_char_{tk.token_end_char_},
-		ws_after{move(tk.ws_after)}
+		end_char_{tk.end_char_},
+		ws_right_{move(tk.ws_right_)}
 	{}
 
-	inline token&operator=(const token&other){
-		ws_before=other.ws_before;
-		token_start_char_=other.token_start_char_;
-		name_=other.name_;
-		token_end_char_=other.token_end_char_;
-		ws_after=other.ws_after;
-		return*this;
-	}
+//	inline token&operator=(const token&other){
+//		ws_left_=other.ws_left_;
+//		start_char_=other.start_char_;
+//		name_=other.name_;
+//		end_char_=other.end_char_;
+//		ws_right_=other.ws_right_;
+//		return*this;
+//	}
 
-	inline void source_to(ostream&os)const{os<<ws_before<<name_<<ws_after;}
+	inline void source_to(ostream&os)const{os<<ws_left_<<name_<<ws_right_;}
 
 	inline void compile_to(ostream&os)const{os<<name_;}
 
@@ -60,16 +62,16 @@ class token final{public:
 
 	inline const string&name()const{return name_;}
 
-	inline size_t token_start_char()const{return token_start_char_;}
+	inline size_t token_start_char()const{return start_char_;}
 
-	inline size_t token_end_char()const{return token_end_char_;}
+	inline size_t token_end_char()const{return end_char_;}
 
 	inline string name_copy()const{return name_;}
 
 	inline bool is_blank()const{
-		const char a=ws_before[0];
+		const char a=ws_left_[0];
 		const char b=name_[0];
-		const char c=ws_after[0];
+		const char c=ws_right_[0];
 		if(a==0 and b==0 and c==0)
 			return true;
 		return false;
@@ -78,11 +80,11 @@ class token final{public:
 
 private:
 //	inline void to_stdout2()const{printf("[%zu]%s[%zu]\n",token_start_char_,name_.get(),token_end_char_);}
-	string ws_before;
-	size_t token_start_char_;
+	string ws_left_;
+	size_t start_char_;
 	string name_;
-	size_t token_end_char_;
-	string ws_after;
+	size_t end_char_;
+	string ws_right_;
 };
 //using up_token=unique_ptr<token>;
 //using vup_tokens=vector<up_token>;
