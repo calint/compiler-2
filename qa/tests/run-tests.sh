c1cd72f6d07453f8e1a4581a148d192ae690199d
calint <calin.tenitchi@gmail.com>
2023-03-04 11:47:44 +0100

    .

--- qa/tests/run-tests.sh
+++ qa/tests/run-tests.sh
@@ -2,16 +2,16 @@
 
 BIN=../../compi
 
-echo -n "t1.baz: "
-$BIN t1.baz > gen.s && nasm -f elf64 gen.s && ld -s -o binary gen.o && ./binary; e=$?
+SRC=t1.baz
+echo -n "$SRC: " && $BIN $SRC > gen.s && nasm -f elf64 gen.s && ld -s -o binary gen.o && ./binary; e=$?
 if test $e -eq 58; then echo ok; else echo nok. expected 58 got $e; exit 1; fi
 
-echo -n "t2.baz: "
-$BIN t2.baz > gen.s && nasm -f elf64 gen.s && ld -s -o binary gen.o && ./binary; e=$?
+SRC=t2.baz
+echo -n "$SRC: " && $BIN $SRC > gen.s && nasm -f elf64 gen.s && ld -s -o binary gen.o && ./binary; e=$?
 if test $e -eq 1; then echo ok; else echo nok. expected 1 got $e; exit 1; fi
 
-echo -n "t3.baz: "
-$BIN t3.baz > gen.s && nasm -f elf64 gen.s && ld -s -o binary gen.o && ./binary; e=$?
+SRC=t3.baz
+echo -n "$SRC: " && $BIN $SRC > gen.s && nasm -f elf64 gen.s && ld -s -o binary gen.o && ./binary; e=$?
 if test $e -eq 7; then echo ok; else echo nok. expected 7 got $e; exit 1; fi
 
 rm binary gen.o gen.s diff.baz
