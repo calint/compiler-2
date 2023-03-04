1229197edc08b07cf1ef8e77a5c2ab4c50612481
calint <calin.tenitchi@gmail.com>
2023-03-04 15:52:37 +0100

    .

---
+++ src/stmt_if_bool_op.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <vector>
+
+#include "statement.hpp"
+
+vector<string> &split(const string &s, char delim, vector<string> &elems);
+vector<string> &split(const string &s, char delim, vector<string> &elems) {
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+vector<string> split(const string &s, char delim);
+vector<string> split(const string &s, char delim) {
+    vector<string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
+
+class stmt_if_bool_op:public statement{public:
+	inline stmt_if_bool_op(const statement&parent,tokenizer&t):
+		statement(parent,t.next_whitespace_token())
+	{
+		// if(not not a=3)   if(a!=3)
+		bool isnot{false};
+		while(true){
+			token tk=t.next_token();
+			if(not tk.is_name("not")){
+				t.pushback_token(tk);
+				break;
+			}
+			isnot=not isnot;
+			nots_.push_back(tk);
+		}
+		isnot_=isnot;
+
+		lhs_=make_unique<expr_ops_list>(*this,t,true);
+		if(lhs_->is_empty())
+			throw compiler_error(parent,"expected left hand side of boolean operation");
+
+		if(t.is_next_char('=')){
+			op_="=";
+		}else if(t.is_next_char('<')){
+			if(t.is_next_char('=')){
+				op_="<=";
+			}else{
+				op_="<";
+			}
+		}else if(t.is_next_char('>')){
+			if(t.is_next_char('=')){
+				op_=">=";
+			}else{
+				op_=">";
+			}
+		}else{
+			throw compiler_error(parent,"expected boolean op");
+		}
+		rhs_=make_unique<expr_ops_list>(*this,t,true);
+		if(rhs_->is_empty())// unary
+			throw compiler_error(*lhs_,"expected right hand side of boolean operation");
+
+
+	}
+
+	inline void source_to(ostream&os)const override{
+		statement::source_to(os);
+
+		for(auto&e:nots_){
+			e.source_to(os);
+		}
+
+		lhs_->source_to(os);
+		os<<op_;
+		rhs_->source_to(os);
+	}
+
+	vector<token>nots_;
+	bool isnot_{false};
+	unique_ptr<expr_ops_list>lhs_;
+	string op_;
+	unique_ptr<expr_ops_list>rhs_;
+};
