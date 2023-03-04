e1e1ff9acd1fd10b04ffc64772749dbe85b3ccc2
calint <calin.tenitchi@gmail.com>
2023-03-04 11:40:38 +0100

    .

--- qa/tests/run-tests.sh
+++ qa/tests/run-tests.sh
@@ -1,13 +1,13 @@
 #/bin/sh
-set -s
+
 BIN=../../compi
 
 echo -n "t1.baz: "
-$BIN t1.baz > gen.s && nasm -f elf64 gen.s && ld -s -o binary gen.o && ./binary
-if test $? -eq 58; then echo ok; else echo nok. expected 58; exit 1; fi
+$BIN t1.baz > gen.s && nasm -f elf64 gen.s && ld -s -o binary gen.o && ./binary; e=$?
+if test $e -eq 58; then echo ok; else echo nok. expected 58 got $e; exit 1; fi
 
 echo -n "t2.baz: "
-$BIN t2.baz > gen.s && nasm -f elf64 gen.s && ld -s -o binary gen.o && ./binary
-if test $? -eq 1; then echo ok; else echo nok. expected 1; exit 1; fi
+$BIN t2.baz > gen.s && nasm -f elf64 gen.s && ld -s -o binary gen.o && ./binary; e=$?
+if test $e -eq 1; then echo ok; else echo nok. expected 1 got $e; exit 1; fi
 
 rm binary gen.o gen.s diff.baz
