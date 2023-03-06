41023323a030abe7b723e2a2ad2f32c800ea3eb7
calint <calin.tenitchi@gmail.com>
2023-03-06 14:29:05 +0100

    .

--- qa/tests/run-tests.sh
+++ qa/tests/run-tests.sh
@@ -2,32 +2,13 @@
 RUN='echo -n "$SRC: " && $BIN $SRC > gen.s && nasm -f elf64 gen.s && ld -s -o gen gen.o && ./gen; e=$?;if test $e -eq $EXP; then echo ok; else echo nok. expected $EXP got $e; exit 1; fi'
 BIN=../../baz
 
-SRC=t1.baz
-EXP=58
-eval $RUN
-
-SRC=t2.baz
-EXP=1
-eval $RUN
-
-SRC=t3.baz
-EXP=7
-eval $RUN
-
-SRC=t4.baz
-EXP=0
-eval $RUN
-
-SRC=t5.baz
-EXP=0
-eval $RUN
-
-SRC=t6.baz
-EXP=0
-eval $RUN
-
-SRC=t7.baz
-EXP=0
-eval $RUN
+SRC=t1.baz && EXP=58 && eval $RUN
+SRC=t2.baz && EXP=1 && eval $RUN
+SRC=t3.baz && EXP=7 && eval $RUN
+SRC=t4.baz && EXP=0 && eval $RUN
+SRC=t5.baz && EXP=0 && eval $RUN
+SRC=t6.baz && EXP=0 && eval $RUN
+SRC=t7.baz && EXP=0 && eval $RUN
+SRC=t8.baz && EXP=0 && eval $RUN
 
 rm gen gen.o gen.s diff.baz
