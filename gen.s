6b595bd0d5a13afd787836e73b03fb5a25e69dd3
calint <calin.tenitchi@gmail.com>
2023-03-07 18:56:09 +0100

    .

--- gen.s
+++ gen.s
@@ -30,116 +30,112 @@ mov esp,stk.end
 ;  [11:11] 4 
 ;  [11:11] d=4 
    mov dword[ebp+12],4
-;  [12:5] var r=a*b+c*d 
-;  [12:9] r=a*b+c*d 
-;  [12:11] a*b+c*d 
-;  [12:11] r=a
-   mov edi,dword[ebp+0]
+;  [12:1] # var r=a+b+c+d 
+;  [13:1] # var r=a*b+c*d 
+;  [14:5] var r=((b+c)*d+c*(a+b))*2 
+;  [14:9] r=((b+c)*d+c*(a+b))*2 
+;  [14:11] ((b+c)*d+c*(a+b))*2 
+;  [14:12] r=((b+c)*d+c*(a+b))
+;  [14:12] ((b+c)*d+c*(a+b))
+;  [14:13] r=(b+c)
+;  [14:13] (b+c)
+;  [14:13] r=b
+   mov edi,dword[ebp+4]
    mov dword[ebp+16],edi
-;  [12:13] r*b
-   mov edi,dword[ebp+16]
-   imul edi,dword[ebp+4]
-   mov dword[ebp+16],edi
-;  [12:16] r+c*d 
-;  [12:16] c*d 
-;  [12:15] edi=c
+;  [14:15] r+c
    mov edi,dword[ebp+8]
-;  [12:17] edi*d 
-   imul edi,dword[ebp+12]
    add dword[ebp+16],edi
-;  [13:1] # var r=((b+c)*d+c*(a+b))*2 
-;  [14:1] # if (a=1 or b=2) and (c=3 or d=4) 
-;  [15:1] # if (a=1 and b=2) or (c=3 and d=4) 
-   if_16_8:
-;  [16:8] ? (a=1 and (b=2 or c=3)) or d=4 
-;  [16:9] ? (a=1 and (b=2 or c=3))
-;  [16:9] ? a=1 
-   cmp_16_9:
+;  [14:18] r*d
+   mov edi,dword[ebp+16]
+   imul edi,dword[ebp+12]
+   mov dword[ebp+16],edi
+;  [14:21] r+c*(a+b)
+;  [14:21] c*(a+b)
+;  [14:20] edi=c
+   mov edi,dword[ebp+8]
+;  [14:23] edi*(a+b)
+;  [14:23] (a+b)
+;  [14:23] esi=a
+   mov esi,dword[ebp+0]
+;  [14:25] esi+b
+   add esi,dword[ebp+4]
+   imul edi,esi
+   add dword[ebp+16],edi
+;  [14:29] r*2 
+   mov edi,dword[ebp+16]
+   imul edi,2
+   mov dword[ebp+16],edi
+;  [15:1] # if (a=1 or b=2) and (c=3 or d=4) 
+;  [16:1] # if (a=1 and b=2) or (c=3 and d=4) 
+   if_17_8:
+;  [17:8] ? (a=1 and (b=2 or c=3)) or d=4 
+;  [17:9] ? (a=1 and (b=2 or c=3))
+;  [17:9] ? a=1 
+   cmp_17_9:
    cmp dword[ebp+0],1
-   jne cmp_16_34
-;  [16:18] ? (b=2 or c=3)
-;  [16:18] ? b=2 
-   cmp_16_18:
+   jne cmp_17_34
+;  [17:18] ? (b=2 or c=3)
+;  [17:18] ? b=2 
+   cmp_17_18:
    cmp dword[ebp+4],2
-   je if_16_8_code
-;  [16:25] ? c=3
-   cmp_16_25:
+   je if_17_8_code
+;  [17:25] ? c=3
+   cmp_17_25:
    cmp dword[ebp+8],3
-   je if_16_8_code  ; opt2
-;  [16:34] ? d=4 
-   cmp_16_34:
+   jne cmp_17_34
+   jmp if_17_8_code
+;  [17:34] ? d=4 
+   cmp_17_34:
    cmp dword[ebp+12],4
-   jne if_16_5_end
-   if_16_8_code:  ; opt1
-;    [17:9] exit(0)
+   jne if_17_5_end
+   jmp if_17_8_code
+   if_17_8_code:
+;    [18:9] exit(0)
 ;      [2:5] mov(ebx,v)
        mov ebx,0
 ;      [3:5] mov(eax,1)
        mov eax,1
 ;      [4:5] int(0x80)
        int 0x80
-     exit_17_9_end:
-   if_16_5_end:
-;  [18:5] var n=10 
-;  [18:9] n=10 
-;  [18:11] 10 
-;  [18:11] n=10 
+     exit_18_9_end:
+   if_17_5_end:
+;  [19:5] var n=10 
+;  [19:9] n=10 
+;  [19:11] 10 
+;  [19:11] n=10 
    mov dword[ebp+20],10
-;  [19:5] loop
-   loop_19_5:
-     if_20_10:
-;    [20:10] ? n=0 
-;    [20:10] ? n=0 
-     cmp_20_10:
+;  [20:5] loop
+   loop_20_5:
+     if_21_10:
+;    [21:10] ? n=0 
+;    [21:10] ? n=0 
+     cmp_21_10:
      cmp dword[ebp+20],0
-     jne if_20_7_end
-     if_20_10_code:  ; opt1
-;      [20:14] break 
-       jmp loop_19_5_end
-     if_20_7_end:
-;    [21:7] var r=((a+b)*c+d)*2 
-;    [21:11] r=((a+b)*c+d)*2 
-;    [21:13] ((a+b)*c+d)*2 
-;    [21:14] r=((a+b)*c+d)
-;    [21:14] ((a+b)*c+d)
-;    [21:15] r=(a+b)
-;    [21:15] (a+b)
-;    [21:15] r=a
-     mov edi,dword[ebp+0]
-     mov dword[ebp+24],edi
-;    [21:17] r+b
-     mov edi,dword[ebp+4]
-     add dword[ebp+24],edi
-;    [21:20] r*c
-     mov edi,dword[ebp+24]
-     imul edi,dword[ebp+8]
-     mov dword[ebp+24],edi
-;    [21:22] r+d
-     mov edi,dword[ebp+12]
-     add dword[ebp+24],edi
-;    [21:25] r*2 
-     mov edi,dword[ebp+24]
-     imul edi,2
-     mov dword[ebp+24],edi
-;    [22:7] n=n-1 
-;    [22:9] n-1 
-;    [22:9] n=n
-;    [22:11] n-1 
+     jne if_21_7_end
+     jmp if_21_10_code
+     if_21_10_code:
+;      [21:14] break 
+       jmp loop_20_5_end
+     if_21_7_end:
+;    [22:1] # var r=((a+b)*c+d)*2 
+;    [23:7] n=n-1 
+;    [23:9] n-1 
+;    [23:9] n=n
+;    [23:11] n-1 
      sub dword[ebp+20],1
-;    [23:7] continue 
-     jmp loop_19_5
-   jmp loop_19_5
-   loop_19_5_end:
-;  [25:5] exit(1)
+;    [24:7] continue 
+     jmp loop_20_5
+   jmp loop_20_5
+   loop_20_5_end:
+;  [26:5] exit(1)
 ;    [2:5] mov(ebx,v)
      mov ebx,1
 ;    [3:5] mov(eax,1)
      mov eax,1
 ;    [4:5] int(0x80)
      int 0x80
-   exit_25_5_end:
+   exit_26_5_end:
 
-;           max regs in use: 1
+;           max regs in use: 2
 ;         max frames in use: 2
 ;          max stack in use: 6
-
