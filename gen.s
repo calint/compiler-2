b96ef6cb888b4ea5545c4e676cc0da755dd9c203
calint <calin.tenitchi@gmail.com>
2023-03-07 18:45:48 +0100

    .

--- gen.s
+++ gen.s
@@ -15,78 +15,58 @@ mov esp,stk.end
 ;  [8:11] 1 
 ;  [8:11] a=1 
    mov dword[ebp+0],1
-;  [9:5] a=a+1 
-;  [9:7] a+1 
-;  [9:7] a=a
-;  [9:9] a+1 
-   add dword[ebp+0],1
-;  [10:5] var b=2 
-;  [10:9] b=2 
-;  [10:11] 2 
-;  [10:11] b=2 
+;  [9:5] var b=2 
+;  [9:9] b=2 
+;  [9:11] 2 
+;  [9:11] b=2 
    mov dword[ebp+4],2
-;  [11:5] var c=3 
-;  [11:9] c=3 
-;  [11:11] 3 
-;  [11:11] c=3 
+;  [10:5] var c=3 
+;  [10:9] c=3 
+;  [10:11] 3 
+;  [10:11] c=3 
    mov dword[ebp+8],3
-;  [12:5] var d=4 
-;  [12:9] d=4 
-;  [12:11] 4 
-;  [12:11] d=4 
+;  [11:5] var d=4 
+;  [11:9] d=4 
+;  [11:11] 4 
+;  [11:11] d=4 
    mov dword[ebp+12],4
-;  [13:5] var r=((b+c)*d+c*(a+b))*2 
-;  [13:9] r=((b+c)*d+c*(a+b))*2 
-;  [13:11] ((b+c)*d+c*(a+b))*2 
-;  [13:12] r=((b+c)*d+c*(a+b))
-;  [13:12] ((b+c)*d+c*(a+b))
-;  [13:13] r=(b+c)
-;  [13:13] (b+c)
-;  [13:13] r=b
-   mov edi,dword[ebp+4]
+;  [12:5] var r=a*b+c*d 
+;  [12:9] r=a*b+c*d 
+;  [12:11] a*b+c*d 
+;  [12:11] r=a
+   mov edi,dword[ebp+0]
    mov dword[ebp+16],edi
-;  [13:15] r+c
-   mov edi,dword[ebp+8]
-   add dword[ebp+16],edi
-;  [13:18] r*d
+;  [12:13] r*b
    mov edi,dword[ebp+16]
+   imul edi,dword[ebp+4]
+   mov dword[ebp+16],edi
+;  [12:16] r+c*d 
+;  [12:16] c*d 
+;  [12:15] edi=c
+   mov edi,dword[ebp+8]
+;  [12:17] edi*d 
    imul edi,dword[ebp+12]
-   mov dword[ebp+16],edi
-;  [13:21] r+c*(a+b)
-;  [13:21] c*(a+b)
-;  [13:20] edi=c
-   mov edi,dword[ebp+8]
-;  [13:23] edi*(a+b)
-;  [13:23] (a+b)
-;  [13:23] esi=a
-   mov esi,dword[ebp+0]
-;  [13:25] esi+b
-   add esi,dword[ebp+4]
-   imul edi,esi
    add dword[ebp+16],edi
-;  [13:29] r*2 
-   mov edi,dword[ebp+16]
-   imul edi,2
-   mov dword[ebp+16],edi
+;  [13:1] # var r=((b+c)*d+c*(a+b))*2 
 ;  [14:1] # if (a=1 or b=2) and (c=3 or d=4) 
 ;  [15:1] # if (a=1 and b=2) or (c=3 and d=4) 
    if_16_8:
-;  [16:8] (a=1 and (b=2 or c=3)) or d=4 
-;  [16:9] (a=1 and (b=2 or c=3))
-;  [16:9] a=1 
+;  [16:8] ? (a=1 and (b=2 or c=3)) or d=4 
+;  [16:9] ? (a=1 and (b=2 or c=3))
+;  [16:9] ? a=1 
    cmp_16_9:
    cmp dword[ebp+0],1
    jne cmp_16_34
-;  [16:18] (b=2 or c=3)
-;  [16:18] b=2 
+;  [16:18] ? (b=2 or c=3)
+;  [16:18] ? b=2 
    cmp_16_18:
    cmp dword[ebp+4],2
    je if_16_8_code
-;  [16:25] c=3
+;  [16:25] ? c=3
    cmp_16_25:
    cmp dword[ebp+8],3
    je if_16_8_code  ; opt2
-;  [16:34] d=4 
+;  [16:34] ? d=4 
    cmp_16_34:
    cmp dword[ebp+12],4
    jne if_16_5_end
@@ -108,8 +88,8 @@ mov esp,stk.end
 ;  [19:5] loop
    loop_19_5:
      if_20_10:
-;    [20:10] n=0 
-;    [20:10] n=0 
+;    [20:10] ? n=0 
+;    [20:10] ? n=0 
      cmp_20_10:
      cmp dword[ebp+20],0
      jne if_20_7_end
@@ -159,7 +139,7 @@ mov esp,stk.end
      int 0x80
    exit_25_5_end:
 
-;           max regs in use: 2
+;           max regs in use: 1
 ;         max frames in use: 2
 ;          max stack in use: 6
 
--- qa/tests/run-tests.sh
+++ qa/tests/run-tests.sh
@@ -13,5 +13,6 @@ SRC=t7.baz && EXP=0 && eval $RUN
 SRC=t8.baz && EXP=0 && eval $RUN
 SRC=t9.baz && EXP=0 && eval $RUN
 SRC=t10.baz && EXP=0 && eval $RUN
+SRC=t11.baz && EXP=14 && eval $RUN
 
 rm gen gen.o gen.s diff.baz
