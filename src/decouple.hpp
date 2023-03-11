6160cdca764994762f56e816318499bac26f0ca9
calint <calin.tenitchi@gmail.com>
2023-03-11 13:06:01 +0100

    .

--- src/decouple.hpp
+++ src/decouple.hpp
@@ -4,5 +4,5 @@ class statement;
 class token;
 class tokenizer;
 
-inline unique_ptr<statement>create_statement_from_tokenizer(statement const&parent,token const&tk,tokenizer&t);
 inline unique_ptr<statement>create_statement_from_tokenizer(statement const&parent,tokenizer&t);
+inline unique_ptr<statement>create_statement_from_tokenizer(statement const&parent,token const&tk,tokenizer&t);
