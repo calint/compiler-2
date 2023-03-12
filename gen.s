e834aa344590ff936c6c3c0a49e6c7e80e5be441
calint <calin.tenitchi@gmail.com>
2023-03-12 18:08:21 +0100

    .

--- gen.s
+++ gen.s
@@ -123,8 +123,6 @@ main:
 ;        [26:16] r14+c(1)
 ;        alloc r13
 ;        [26:16] c(1)
-         push rbx
-         push r15
          push r14
          push r13
          push 1
@@ -132,17 +130,12 @@ main:
          add rsp,8
          pop r13
          pop r14
-         pop r15
-         pop rbx
          mov r13,rax
          add r14,r13
 ;        free r13
 ;        [26:21] r14+c(x+y)
 ;        alloc r13
 ;        [26:21] c(x+y)
-         push rbx
-         push r15
-         push r14
          push r13
 ;        alloc r12
 ;          [26:23] x+y
@@ -155,9 +148,6 @@ main:
          call c
          add rsp,8
          pop r13
-         pop r14
-         pop r15
-         pop rbx
          mov r13,rax
          add r14,r13
 ;        free r13
