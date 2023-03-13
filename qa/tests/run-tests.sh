341ef181c900a0ca8e825c52c626f83280f885ad
calint <calin.tenitchi@gmail.com>
2023-03-13 14:25:39 +0100

    .

--- qa/tests/run-tests.sh
+++ qa/tests/run-tests.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 RUN='echo -n "$SRC: " && $BIN $SRC > gen.s && nasm -f elf64 gen.s && ld -s -o gen gen.o && ./gen; e=$?;if test $e -eq $EXP; then echo ok; else echo FAILED. expected $EXP got $e; exit 1; fi'
-RUNDIFF='echo -n "$SRC: " && $BIN $SRC > gen.s && nasm -f elf64 gen.s && ld -s -o gen gen.o && ./gen > out; if cmp -s out ${SRC%.*}.out; then echo ok; else echo FAILED; exit 1; fi'
+DIFF='echo -n "$SRC: " && $BIN $SRC > gen.s && nasm -f elf64 gen.s && ld -s -o gen gen.o && ./gen > out; if cmp -s out ${SRC%.*}.out; then echo ok; else echo FAILED; exit 1; fi'
 BIN=../../baz
 
 SRC=t1.baz && EXP=58 && eval $RUN
@@ -18,10 +18,10 @@ SRC=t11.baz && EXP=14 && eval $RUN
 SRC=t12.baz && EXP=24 && eval $RUN
 SRC=t13.baz && EXP=2 && eval $RUN
 SRC=t14.baz && EXP=0 && eval $RUN
-SRC=t15.baz && eval $RUNDIFF
-SRC=t16.baz && eval $RUNDIFF
-SRC=t17.baz && eval $RUNDIFF
-SRC=t18.baz && eval $RUNDIFF
+SRC=t15.baz && eval $DIFF
+SRC=t16.baz && eval $DIFF
+SRC=t17.baz && eval $DIFF
+SRC=t18.baz && eval $DIFF
 SRC=t19.baz && EXP=120 && eval $RUN
 SRC=t20.baz && EXP=7 && eval $RUN
 SRC=t21.baz && EXP=12 && eval $RUN
