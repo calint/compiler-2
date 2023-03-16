9a568a49230aa3bfc65455a578153882b5fa7bcb
calint <calin.tenitchi@gmail.com>
2023-03-16 12:45:58 +0100

    .

--- src/decouple.hpp
+++ src/decouple.hpp
@@ -1,8 +1,4 @@
 #pragma once
 
-class statement;
-class token;
-class tokenizer;
-
 inline unique_ptr<statement>create_statement_from_tokenizer(tokenizer&t);
 inline unique_ptr<statement>create_statement_from_tokenizer(token tk,tokenizer&t);
