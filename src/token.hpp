3dcd3d707ddf26330940471c492a2c826074672d
calint <calin.tenitchi@gmail.com>
2023-03-16 12:04:36 +0100

    .

--- src/token.hpp
+++ src/token.hpp
@@ -7,18 +7,18 @@ class token final{
 public:
 	inline token():
 		ws_left_{},
-		start_char_{},
+		start_ix_{},
 		name_{},
-		end_char_{},
+		end_ix_{},
 		ws_right_{},
 		is_str_{}
 	{}
 
-	inline token(string wb,size_t n,string tk,size_t n2,string wa,bool is_str=false):
+	inline token(string wb,size_t start_ix,string tk,size_t end_ix,string wa,bool is_str=false):
 		ws_left_{wb},
-		start_char_{n},
+		start_ix_{start_ix},
 		name_{tk},
-		end_char_{n2},
+		end_ix_{end_ix},
 		ws_right_{wa},
 		is_str_{is_str}
 	{
@@ -27,9 +27,9 @@ public:
 
 	inline token(const token&tk):
 		ws_left_{tk.ws_left_},
-		start_char_{tk.start_char_},
+		start_ix_{tk.start_ix_},
 		name_{tk.name_},
-		end_char_{tk.end_char_},
+		end_ix_{tk.end_ix_},
 		ws_right_{tk.ws_right_},
 		is_str_{tk.is_str_}
 	{
@@ -39,9 +39,9 @@ public:
 	token&operator=(const token&other){
 		// cout<<"operator =\n";
 		ws_left_=other.ws_left_;
-		start_char_=other.start_char_;
+		start_ix_=other.start_ix_;
 		name_=other.name_;
-		end_char_=other.end_char_;
+		end_ix_=other.end_ix_;
 		ws_right_=other.ws_right_;
 		is_str_=other.is_str_;
 		return*this;
@@ -49,9 +49,9 @@ public:
 
 	inline token(token&&tk):
 		ws_left_{move(tk.ws_left_)},
-		start_char_{tk.start_char_},
+		start_ix_{tk.start_ix_},
 		name_{move(tk.name_)},
-		end_char_{tk.end_char_},
+		end_ix_{tk.end_ix_},
 		ws_right_{move(tk.ws_right_)},
 		is_str_{tk.is_str_}
 	{
@@ -79,9 +79,9 @@ public:
 
 	inline const string&name()const{return name_;}
 
-	inline size_t char_index()const{return start_char_;}
+	inline size_t char_index()const{return start_ix_;}
 
-	inline size_t char_index_end()const{return end_char_;}
+	inline size_t char_index_end()const{return end_ix_;}
 
 	inline bool is_blank()const{
 		if(ws_left_.empty() and name_.empty() and ws_right_.empty())
@@ -97,9 +97,9 @@ public:
 
 private:
 	string ws_left_;
-	size_t start_char_{0};
+	size_t start_ix_{0};
 	string name_;
-	size_t end_char_{0};
+	size_t end_ix_{0};
 	string ws_right_;
 	bool is_str_{false};
 };
