f67ee18029479d70355844274d8783f14339472c
calint <calin.tenitchi@gmail.com>
2023-03-18 23:25:22 +0100

    .

--- gen-m.s
+++ gen-m.s
@@ -14,29 +14,24 @@ mov rbp,rsp
 jmp main
 main:
    mov qword[rbp-8],-1
-   mov qword[rbp-16],-2
-   mov qword[rbp-24],1
    mov r15,qword[rbp-8]
    neg r15
-   imul r15,qword[rbp-16]
+   imul r15,qword[rbp-8]
    neg r15
-   mov r14,3
-   imul r14,qword[rbp-8]
-   neg r14
-   add r15,r14
-   add qword[rbp-24],r15
-   if_11_8:
-   cmp_11_8:
-   cmp qword[rbp-24],6
-   jne if_11_5_end
-   jmp if_11_8_code
-   if_11_8_code:
+   neg r15
+   mov qword[rbp-16],r15
+   if_10_8:
+   cmp_10_8:
+   cmp qword[rbp-16],-1
+   jne if_10_5_end
+   jmp if_10_8_code
+   if_10_8_code:
        mov rbx,0
        mov rax,1
        int 0x80
-     exit_12_9_end:
-   if_11_5_end:
+     exit_11_9_end:
+   if_10_5_end:
      mov rbx,1
      mov rax,1
      int 0x80
-   exit_13_5_end:
+   exit_12_5_end:
--- qa/tests/run-tests.sh
+++ qa/tests/run-tests.sh
@@ -37,5 +37,6 @@ SRC=t30.baz && EXP=0 && eval $RUN
 SRC=t31.baz && EXP=0 && eval $RUN
 SRC=t32.baz && EXP=0 && eval $RUN
 SRC=t33.baz && EXP=0 && eval $RUN
+SRC=t34.baz && EXP=0 && eval $RUN
 
 rm gen gen.o gen.s diff.baz out
