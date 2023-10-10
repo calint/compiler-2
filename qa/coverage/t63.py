8e4c05b4106ad5d325a6e122551d35439cc2e2f0
calint <calin.tenitchi@gmail.com>
2023-10-10 15:16:34 +0200

    .

--- qa/coverage/t63.py
+++ qa/coverage/t63.py
@@ -2,26 +2,26 @@
 
 import subprocess
 
-process = subprocess.Popen(['./gen'],
+p = subprocess.Popen(['./gen'],
                            stdin=subprocess.PIPE,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE,
                            universal_newlines=True)
 
-print(process.stdout.readline(), end='') # hello
-print(process.stdout.readline(), end='') # enter name
-process.stdin.write('bob\n')
-process.stdin.flush()
-print(process.stdout.readline(), end='') # not a name
-print(process.stdout.readline(), end='') # enter name
-process.stdin.write('charlie\n')
-process.stdin.flush()
-print(process.stdout.readline(), end='') # hello charlie
-print(process.stdout.readline(), end='') # enter name
-process.stdin.write('\n')
-process.stdin.flush()
+print(p.stdout.readline(), end='') # hello
+print(p.stdout.readline(), end='') # enter name
+p.stdin.write('bob\n')
+p.stdin.flush()
+print(p.stdout.readline(), end='') # not a name
+print(p.stdout.readline(), end='') # enter name
+p.stdin.write('charlie\n')
+p.stdin.flush()
+print(p.stdout.readline(), end='') # hello charlie
+print(p.stdout.readline(), end='') # enter name
+p.stdin.write('\n')
+p.stdin.flush()
 
-process.stdin.close()
-process.stdout.close()
-process.stderr.close()
-process.wait()
+p.stdin.close()
+p.stdout.close()
+p.stderr.close()
+p.wait()
