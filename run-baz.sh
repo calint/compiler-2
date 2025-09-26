51e43cefcfc47bdd4f1e6a40d493bffd01564a9c
calint <calin.tenitchi@gmail.com>
2025-09-26 14:42:58 +0700

    .

--- run-baz.sh
+++ run-baz.sh
@@ -9,12 +9,12 @@ cd $(dirname "$0")
 
 SEP="--------------------------------------------------------------------------------"
 echo $SEP
-./baz $PRG > gen.s &&
-nasm -f elf64 gen.s &&
-ld -s -o gen gen.o &&
-cat gen.s | grep -v -e'^\s*;.*$' -e'^\s*$' > gen-without-comments.s &&
-ls --color -la gen.s gen-without-comments.s gen &&
-echo $SEP &&
-./gen &&
-echo $SEP
+./baz $PRG >gen.s &&
+  nasm -f elf64 gen.s &&
+  ld -s -o gen gen.o &&
+  cat gen.s | grep -v -e'^\s*;.*$' -e'^\s*$' >gen-without-comments.s &&
+  ls --color -la gen.s gen-without-comments.s gen &&
+  echo $SEP &&
+  ./gen &&
+  echo $SEP
 echo returned: $?
