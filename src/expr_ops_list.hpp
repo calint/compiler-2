0a83ed6f8a523e1ee3fd9456464a10510460d06b
calint <calin.tenitchi@gmail.com>
2025-09-29 15:39:42 +0700

    .

--- src/expr_ops_list.hpp
+++ src/expr_ops_list.hpp
@@ -7,13 +7,13 @@
 #include "expression.hpp"
 #include "toc.hpp"
 
-// a list of elements / lists connected by operators instead of true
+// a list of elements / lists connected by operators instead of tree
 // quirky 1'st pass with trivial second pass compilation
 class expr_ops_list final : public expression {
     bool enclosed_{};                               //  (a+b) vs a+b
-    std::vector<std::unique_ptr<statement>> exprs_; // expression list
-    std::vector<char> ops_; // operators between elements of the vector
-    unary_ops uops_;        // unary ops e.g. ~(a+b)
+    std::vector<std::unique_ptr<statement>> exprs_; // expressions list
+    std::vector<char> ops_; // operators between elements in the vector
+    unary_ops uops_;        // unary ops for all result e.g. ~(a+b)
 
   public:
     expr_ops_list(toc& tc, tokenizer& tz, const bool in_args = false,
@@ -26,7 +26,7 @@ class expr_ops_list final : public expression {
 
         // read first expression e.g. =-a/-(b+1)
         if (first_expression) {
-            // called in a recursion with first expression provided
+            // called in recursion with first expression provided
             exprs_.emplace_back(std::move(first_expression));
         } else {
             // check if new recursion is necessary
@@ -48,20 +48,12 @@ class expr_ops_list final : public expression {
         while (true) { // +a  +3
             // if end of subexpression
             if (enclosed_ and tz.is_next_char(')')) {
+                // break recursion
                 break;
             }
 
-            // if parsed in a function call argument list or in a boolean
-            // expression
+            // if parsed in a function call argument list
             if (in_args) {
-                // if in boolean expression check for '<' and '>' but not '<<'
-                // and '>>'
-                if (tz.is_peek_char('<') and not tz.is_peek_char2('<')) {
-                    break;
-                }
-                if (tz.is_peek_char('>') and not tz.is_peek_char2('>')) {
-                    break;
-                }
                 // if in arguments exit when ',' or ')' is found
                 if (tz.is_peek_char(',') or tz.is_peek_char(')')) {
                     break;
@@ -116,8 +108,10 @@ class expr_ops_list final : public expression {
                 continue;
             }
 
+            // if precedence same or lower continue building this list
             precedence = next_precedence;
             const char ch{tz.next_char()}; // read the peeked operator
+            // handle the two character operator shift
             if (ch == '<') {
                 if (tz.next_char() != '<') {
                     throw compiler_exception(tz, "expected operator '<<'");
@@ -129,20 +123,26 @@ class expr_ops_list final : public expression {
             }
 
             // check if next statement is a sub-expression
-            const unary_ops uo{tz};
+            // e.g. -(a + b)
+            const unary_ops uo{tz}; // read the unary ops, in this case '-'
+            // is it a sub-expression?
             if (tz.is_next_char('(')) {
-                // sub-expression, recurse
+                // yes, recurse and forward the unary ops to be applied on the
+                // whole sub-expression
                 exprs_.emplace_back(
                     std::make_unique<expr_ops_list>(tc, tz, in_args, true, uo));
                 continue;
             }
 
-            // not sub-expression
+            // not sub-expression, push back unary ops because those belong to
+            // the next element
+            // e.g. -a+b
             uo.put_back(tz);
 
-            // read statement
+            // read next element
             std::unique_ptr<statement> st{
                 create_statement_from_tokenizer(tc, tz)};
+            // handle the case of malformed code when stream has ended
             if (st->tok().is_empty()) {
                 throw compiler_exception(st->tok(),
                                          "unexpected '" +
@@ -150,6 +150,8 @@ class expr_ops_list final : public expression {
             }
             // move statement to list
             exprs_.emplace_back(std::move(st));
+
+            // next op + element or sub-expression
         }
     }
 
@@ -175,7 +177,7 @@ class expr_ops_list final : public expression {
             const char op{ops_.at(i)};
             os << op;
             if (op == '<' or op == '>') {
-                // << or >>
+                // handle case << and >>
                 os << op;
             }
             exprs_.at(i + 1)->source_to(os);
@@ -191,17 +193,20 @@ class expr_ops_list final : public expression {
 
         const ident_info dst_info{tc.make_ident_info(tok(), dst, false)};
 
+        // is destination a register?
         if (dst_info.is_register()) {
+            // yes, compile with result placed in it
             do_compile(tc, os, indent, dst_info);
             return;
         }
 
-        // compile with and without scratch register
-        // try without scratch register
+        // compile with and without scratch register to find best compilation
+
+        // without scratch register
         std::stringstream ss1;
         do_compile(tc, ss1, indent, dst_info);
 
-        // try with scratch register
+        // with scratch register
         std::stringstream ss2;
         const std::string& reg{tc.alloc_scratch_register(tok(), ss2, indent)};
         const ident_info dest_reg{tc.make_ident_info(tok(), reg, false)};
@@ -228,21 +233,25 @@ class expr_ops_list final : public expression {
     }
 
     [[nodiscard]] auto get_unary_ops() const -> const unary_ops& override {
-        if (exprs_.size() == 1) { //? why the unary ops of first expression
+        // is this list one element?
+        if (exprs_.size() == 1) {
+            // then the unary ops are on the first element
             return exprs_.at(0)->get_unary_ops();
         }
-
+        // in multi-element list, unary ops for all are on the current list
+        // element
         return uops_;
     }
 
     [[nodiscard]] auto get_type() const -> const type& override {
         assert(not exprs_.empty());
-        //? hack  find the size of the largest integral element
+        // return size of first element
+        //? todo.  find the size of the largest integral element
         return exprs_.at(0)->get_type();
     }
 
     [[nodiscard]] auto is_expression() const -> bool override {
-        // if unary operators then it will need to compile expression
+        // if unary operators on list then it will need to compile expression
         if (not uops_.is_empty()) {
             return true;
         }
