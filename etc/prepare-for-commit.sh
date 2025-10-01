63e79142c6c09f1b7177390a6492d457519aed43
calint <calin.tenitchi@gmail.com>
2025-10-01 15:20:34 +0700

    .

--- etc/prepare-for-commit.sh
+++ etc/prepare-for-commit.sh
@@ -3,9 +3,11 @@ set -e
 cd $(dirname "$0")
 
 cd ..
+
+cp -a etc/prog.baz.bak prog.baz
 clang-format -i src/*
-cp prog.baz.bak prog.baz
 qa/coverage/run-tests.sh
-./make.sh
-etc/README.md.make.sh
 qa/lint/clang-tidy.sh
+./make.sh build
+etc/README.md.make.sh
+./make.sh
