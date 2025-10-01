f904497c330098e1167aec30adbffccd603e6f72
calint <calin.tenitchi@gmail.com>
2025-10-01 15:43:06 +0700

    .

--- run-baz.sh
+++ run-baz.sh
@@ -5,7 +5,7 @@ cd $(dirname "$0")
 PRG=${1:-prog.baz}
 SEP="..  . .. . . . . .  ......... . ..  . .  . . ....  . .. ....... . . "
 echo $SEP
-./baz prog.baz >gen.s
+./baz $PRG >gen.s
 ls --color -la gen.s gen-without-comments.s gen
 grep -v -e'^\s*;.*$' -e'^\s*$' gen.s >gen-without-comments.s
 nasm -f elf64 gen.s
