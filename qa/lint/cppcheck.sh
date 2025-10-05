e29dc3a6ca0975c383e1c6987b991d8cd0f457e4
calint <calin.tenitchi@gmail.com>
2025-10-05 14:47:07 +0700

    .

--- qa/lint/cppcheck.sh
+++ qa/lint/cppcheck.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # tools:
-#   cppcheck: 2.10
+#   cppcheck: 2.18.3
 cd $(dirname "$0")
 
 SRC=../../src/main.cpp
 
 date | tee cppcheck.log
-#    --suppress=knownConditionTrueFalse \
-cppcheck --enable=all --suppress=missingIncludeSystem \
-  $SRC 2>&1 | tee -a cppcheck.log
+
+cppcheck --enable=all --suppress=missingIncludeSystem $SRC 2>&1 | tee -a cppcheck.log
+
 date | tee -a clang-tidy.log
