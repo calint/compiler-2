078995a9cbf98179cb3b83691cfdedaebbdcf287
calint <calin.tenitchi@gmail.com>
2025-10-06 17:38:37 +0700

    .

--- etc/run-tests-and-print-peak-mem-usage.sh
+++ etc/run-tests-and-print-peak-mem-usage.sh
@@ -2,11 +2,11 @@
 #set -e
 cd $(dirname "$0")
 
-cd ..
+cd ../qa/coverage/
 
-for file in qa/coverage/*.baz; do
+for file in *.baz; do
   echo -n "$file: "
-  valgrind --tool=massif --massif-out-file=massif.out ./baz "$file" >/dev/null 2>/dev/null
-  grep mem_heap_B massif.out | sort -g | tail -n 1
+  valgrind --tool=massif --massif-out-file=massif.out ../../baz "$file" >/dev/null 2>/dev/null
+  grep mem_heap_B massif.out | sort -r | head -n 1 | awk '{printf "%s ", $2; print $1}'
   rm massif.out
 done
