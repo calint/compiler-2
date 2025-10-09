79582321fe3e4520a029ee89dde37821235db58d
calint <calin.tenitchi@gmail.com>
2025-10-09 13:27:59 +0700

    .

--- etc/tag-and-push.sh
+++ etc/tag-and-push.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
-cd $(dirname "$0")
+cd "$(dirname "$0")"
 
 cd ..
-TAG=$(date "+%Y-%m-%d--%H-%M") && git tag $TAG && git push origin $TAG
+TAG=$(date "+%Y-%m-%d--%H-%M")
+if [ -n "$1" ]; then
+  TAG="${TAG}--$1"
+fi
+
+echo "Creating tag: $TAG"
+
+git tag "$TAG"
+git push origin "$TAG"
