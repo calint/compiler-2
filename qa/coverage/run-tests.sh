1a54154a40f44c734664a207f3d2e300ab74663c
calint <calin.tenitchi@gmail.com>
2025-10-11 18:50:21 +0700

    .

--- qa/coverage/run-tests.sh
+++ qa/coverage/run-tests.sh
@@ -1,158 +1,114 @@
-#/bin/sh
+#!/bin/sh
 # tools:
 #   llvm-profdata: ?
 #        llvm-cov: 15.0.7
 #         genhtml: 1.16
 set -e
+
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd $SCRIPT_DIR
+cd "$SCRIPT_DIR"
 
 ../../make.sh build prof asan
-
 BIN='../../baz'
 
 export UBSAN_OPTIONS="print_stacktrace=1"
-
 export ASAN_OPTIONS="fast_unwind_on_fatal=0:print_stacktrace=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1:detect_leaks=1:halt_on_error=1:external_symbolizer=1"
 export ASAN_SYMBOLIZER_PATH="$(which llvm-symbolizer)"
 export ASAN_SYMBOLIZE=1
 
-RUN() {
-  echo -n "$SRC: "
-
+# Common: compile and assemble
+compile_and_build() {
   rm -f error.log
-
   LLVM_PROFILE_FILE="${SRC%.*}.profraw" $BIN "$SRC.baz" 2>error.log >gen.s
-
   if [ $? -ne 0 ]; then
     echo "compiler failed. see 'error.log' and 'gen.s'" >&2
     exit 1
   fi
-
   nasm -f elf64 gen.s
   ld -s -o gen gen.o
+}
+
+# Common: compare output with expected
+check_output() {
+  local expected="$1"
+  if cmp -s out "$expected"; then
+    echo ok
+  else
+    echo "FAILED. output differs. see: diff $expected out"
+    exit 1
+  fi
+}
+
+# Test with exit code
+RUN() {
+  echo -n "$SRC: "
+  compile_and_build
 
   set +e
   ./gen
-  e=$?
+  local exit_code=$?
   set -e
 
-  if test $e -eq $EXP; then
+  if [ $exit_code -eq $EXP ]; then
     echo ok
   else
-    echo "FAILED. expected $EXP got $e"
+    echo "FAILED. expected $EXP got $exit_code"
     exit 1
   fi
 }
 
+# Test with output comparison (no input)
 DIFF() {
   echo -n "$SRC: "
-
-  rm -f error.log
-
-  LLVM_PROFILE_FILE="${SRC%.*}.profraw" $BIN "$SRC.baz" 2>error.log >gen.s
-
-  if [ $? -ne 0 ]; then
-    echo "compiler failed. see 'error.log' and 'gen.s'" >&2
-    exit 1
-  fi
-
-  nasm -f elf64 gen.s
-  ld -s -o gen gen.o
-
+  compile_and_build
   ./gen >out
-
-  if cmp -s out "${SRC%.*}.out"; then
-    echo ok
-  else
-    echo "FAILED. output differs. see: diff ${SRC%.*}.out out"
-    exit 1
-  fi
+  check_output "${SRC%.*}.out"
 }
 
+# Test with input file
 DIFFINP() {
   echo -n "$SRC: "
-
-  rm -f error.log
-
-  LLVM_PROFILE_FILE="${SRC%.*}.profraw" $BIN "$SRC.baz" 2>error.log >gen.s
-
-  if [ $? -ne 0 ]; then
-    echo "compiler failed. see 'error.log' and 'gen.s'" >&2
-    exit 1
-  fi
-
-  nasm -f elf64 gen.s
-  ld -s -o gen gen.o
-
+  compile_and_build
   ./gen <"${SRC%.*}.in" >out
-
-  if cmp -s out "${SRC%.*}.out"; then
-    echo ok
-  else
-    echo FAILED. output differs. see: diff ${SRC%.*}.out out
-    exit 1
-  fi
+  check_output "${SRC%.*}.out"
 }
 
+# Test with line-by-line input
 DIFFINP2() {
   echo -n "$SRC: "
+  compile_and_build
 
-  rm -f error.log
-
-  LLVM_PROFILE_FILE="${SRC%.*}.profraw" $BIN "$SRC.baz" 2>error.log >gen.s
-
-  if [ $? -ne 0 ]; then
-    echo "compiler failed. see 'error.log' and 'gen.s'" >&2
-    exit 1
-  fi
-
-  nasm -f elf64 gen.s
-  ld -s -o gen gen.o
-
-  while read line; do
+  while read -r line; do
     echo "$line"
-    sleep 0.001 # arbitrary small wait for that single line to be received by `read`
+    sleep 0.001 # Small wait for single line to be received by `read`
   done <"${SRC%.*}.in" | ./gen >out
 
-  if cmp -s out "${SRC%.*}.out"; then
-    echo ok
-  else
-    echo FAILED. output differs. see: diff ${SRC%.*}.out out
-    exit 1
-  fi
+  check_output "${SRC%.*}.out"
 }
 
+# Test with Python script
 DIFFPY() {
   echo -n "$SRC: "
-
-  rm -f error.log
-
-  LLVM_PROFILE_FILE="${SRC%.*}.profraw" $BIN "$SRC.baz" 2>error.log >gen.s
-
-  nasm -f elf64 gen.s
-  ld -s -o gen gen.o
-
+  compile_and_build
   "./${SRC%.*}.py" >out
-
-  if cmp -s out "${SRC%.*}.out"; then
-    echo ok
-  else
-    echo FAILED. output differs. see: diff ${SRC%.*}.out out
-    exit 1
-  fi
+  check_output "${SRC%.*}.out"
 }
 
+# Run all test cases
 source "$SCRIPT_DIR/run-tests-cases.sh"
 
-rm gen gen.o gen.s diff.baz out error.log
+# Cleanup
+rm -f gen gen.o gen.s diff.baz out error.log
 
-# process coverage data
+# Process coverage data
 llvm-profdata merge -o baz.profdata -sparse $(ls *.profraw)
 llvm-cov export --format=lcov --instr-profile baz.profdata --object $BIN >lcov.info
-# generate report
+
+# Generate report
 genhtml --ignore-errors unsupported -quiet lcov.info --output-directory report/
+
 echo
-echo coverage report generated in "$(realpath "report/")"
+echo "coverage report generated in $(realpath "report/")"
 echo
-rm *.profraw baz.profdata lcov.info
+
+rm -f *.profraw baz.profdata lcov.info
