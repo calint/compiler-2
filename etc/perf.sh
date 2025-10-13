8275e0ec58fab7b7e3817cf97c96cf9da2d76602
calint <calin.tenitchi@gmail.com>
2025-10-13 08:24:10 +0700

    .

---
+++ etc/perf.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+cd $(dirname "$0")
+cd ..
+cd build
+rm -rf *
+cmake ..
+make
+cd ..
+sudo perf record -g build/baz $@
+sudo chmod +r perf.data
+echo
+perf report --stdio --no-children --sort symbol,overhead |
+  grep -E "^\s+[0-9]+\.[0-9]+%\s+\[.*\]" |
+  sed 's/\s\+-\s\+-.*$//' |
+  grep -v "\[k\]" |
+  sort -r
