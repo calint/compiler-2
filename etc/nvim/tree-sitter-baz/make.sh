1d5c39ce3caaef000750c0aa6f5a035b23a23b2b
calint <calin.tenitchi@gmail.com>
2025-10-18 22:29:15 +0700

    .

--- etc/nvim/tree-sitter-baz/make.sh
+++ etc/nvim/tree-sitter-baz/make.sh
@@ -7,5 +7,5 @@ tree-sitter generate
 cc -o baz.so -shared src/parser.c -I./node_modules/tree-sitter/lib/include -fPIC
 mv -f baz.so ~/.local/share/nvim/site/parser/
 cp queries/highlights.scm ~/.local/share/nvim/site/queries/baz/highlights.scm
-# copy to `tree-sitter-baz`` project
+# copy to `tree-sitter-baz` project
 cp queries/highlights.scm ../../../../tree-sitter-baz/queries/baz/
