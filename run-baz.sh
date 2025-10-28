b42d65dfb7f531b30a7fb37f7784a00c55176152
calint <calin.tenitchi@gmail.com>
2025-10-28 13:13:27 +0700

    .

--- run-baz.sh
+++ run-baz.sh
@@ -10,8 +10,8 @@ $CMD >gen.s
 echo $SEP
 grep -v -e'^\s*;.*$' -e'^\s*$' gen.s >gen-without-comments.s
 grep -v -e'^\s*;.*$' -e'^\s*$' -e'^.*:\s*$' gen.s | wc | awk '{print "instructions: " $1}'
-grep -E '^\s*jmp\s.*.*$' gen.s | wc | awk '{print "jumps: " $1}'
-grep -E '^\s*j[a-z]{1,2}\s' gen.s | grep -v '^\s*jmp\s' | wc | awk '{print "conditional: " $1}'
+grep -E '^\s*jmp\s.*.*$' gen.s | wc | awk '{print "jmp: " $1}'
+grep -E '^\s*j[a-z]{1,2}\s' gen.s | grep -v '^\s*jmp\s' | wc | awk '{print "jcc: " $1}'
 echo $SEP
 nasm -f elf64 gen.s
 ld -s -o gen gen.o
