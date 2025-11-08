32fe9794b52a7cad971d3fd6a88afc9af583d6a6
calint <calin.tenitchi@gmail.com>
2025-11-08 07:53:12 +0700

    .

--- src/stmt_builtin_array_size_of.hpp
+++ src/stmt_builtin_array_size_of.hpp
@@ -70,8 +70,7 @@ class stmt_builtin_array_size_of final : public expression {
             throw compiler_exception{tok(), "destination must be type i64"};
         }
 
-        const ident_info src_info{
-            tc.make_ident_info(stmt_ident_.tok(), stmt_ident_.identifier())};
+        const ident_info src_info{tc.make_ident_info(stmt_ident_)};
 
         if (not src_info.is_var()) {
             throw compiler_exception{stmt_ident_.first_token(),
