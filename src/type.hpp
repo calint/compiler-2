664e27a626ad7ed1e1e2e438d4fd40603ce459a7
calint <calin.tenitchi@gmail.com>
2026-09-09 00:12:16 +0700

    .

--- src/type.hpp
+++ src/type.hpp
@@ -1,5 +1,6 @@
 #pragma once
 // reviewed: 2025-09-28
+//           2026-09-09
 
 #include <format>
 #include <ranges>
@@ -15,7 +16,7 @@
 class type;
 
 struct type_field {
-    std::string name;
+    std::string name;       //
     const type* type_ptr{}; // element type
     size_t size{};          // total size in bytes of all elements
     size_t array_size{};    // array size in elements
@@ -25,7 +26,7 @@ struct type_field {
 
 class type final {
     std::string name_;
-    size_t size_{};
+    size_t size_{}; // total size of type in bytes
     std::vector<type_field> fields_;
     bool is_built_in_{};
 
@@ -46,6 +47,7 @@ class type final {
                    const bool is_array, const size_t array_size) -> void {
 
         const size_t size{tp.size_ * (is_array ? array_size : 1)};
+
         fields_.emplace_back(std::string{name}, &tp, size, array_size, is_array,
                              size_);
 
@@ -71,24 +73,31 @@ class type final {
                                 std::vector<const type*>& type_path) const
         -> ident_info {
 
-        const type* tp{this};
         type_path.emplace_back(this);
         size_t offset{};
         bool is_array{var.is_array};
         size_t array_size{var.array_size};
+        const type* tp{this};
+
         for (const auto& field_name : path | std::views::drop(1)) {
-            const type_field& fld{tp->field(tk, field_name)};
-            offset += fld.offset;
-            tp = fld.type_ptr;
+            // note: drop 1 because the first element is retrieved outside the
+            //       loop
+
+            const type_field& tf{tp->field(tk, field_name)};
+            offset += tf.offset;
+            tp = tf.type_ptr;
+            is_array = tf.is_array;
+            array_size = tf.array_size;
             type_path.emplace_back(tp);
-            is_array = fld.is_array;
-            array_size = fld.array_size;
         }
+
         const int stack_idx{var.stack_idx + static_cast<int>(offset)};
 
         // find the first built-in type to have a valid operand size for the
         // address
+
         const type* tp_first_field{tp};
+
         while (not tp_first_field->is_built_in()) {
             tp_first_field = tp_first_field->fields_[0].type_ptr;
         }
@@ -117,15 +126,18 @@ class type final {
     field_offset(const token& src_loc_tk,
                  const std::span<const std::string> path) const -> size_t {
 
-        const type* tp{this};
         size_t offset{};
+        const type* tp{this};
+
         for (const auto& field_name : path | std::views::drop(1)) {
             // note: drop 1 because the first element is retrieved outside the
-            // loop
-            const type_field& fld{tp->field(src_loc_tk, field_name)};
-            offset += fld.offset;
-            tp = fld.type_ptr;
+            //       loop
+
+            const type_field& tf{tp->field(src_loc_tk, field_name)};
+            offset += tf.offset;
+            tp = tf.type_ptr;
         }
+
         return offset;
     }
 
@@ -142,6 +154,7 @@ class type final {
     }
 
     // note: duplicate responsibility with `toc` because of circular dependency
+
     [[nodiscard]] static auto get_size_specifier(const token& tk,
                                                  const size_t size)
         -> std::string_view {
--- src/unary_ops.hpp
+++ src/unary_ops.hpp
@@ -1,5 +1,6 @@
 #pragma once
 // reviewed: 2025-09-28
+//           2026-09-09
 
 #include <cstdint>
 #include <ostream>
@@ -14,11 +15,11 @@
 class toc;
 
 class unary_ops final {
-    token ws_before_; // whitespace before the ops
-    std::vector<char> ops_;
+    token ws1_;             // whitespace before the ops
+    std::vector<char> ops_; // the unary ops as character
 
   public:
-    explicit unary_ops(tokenizer& tz) : ws_before_{tz.next_whitespace_token()} {
+    explicit unary_ops(tokenizer& tz) : ws1_{tz.next_whitespace_token()} {
         while (true) {
             if (tz.is_next_char('~')) {
                 ops_.emplace_back('~');
@@ -47,11 +48,11 @@ class unary_ops final {
         for (const char op : ops_ | std::views::reverse) {
             tz.put_back_char(op);
         }
-        tz.put_back_token(ws_before_);
+        tz.put_back_token(ws1_);
     }
 
     auto source_to(std::ostream& os) const -> void {
-        ws_before_.source_to(os);
+        ws1_.source_to(os);
         source_to_without_whitespace(os);
     }
 
@@ -63,8 +64,8 @@ class unary_ops final {
 
     // implemented in 'main.cpp'
     // solves circular reference: unary_ops -> toc -> statement -> unary_ops
-    inline auto compile(toc& tc, std::ostream& os, size_t indnt,
-                        std::string_view dst_info) const -> void;
+    inline auto compile(toc& tc, std::ostream& os, const size_t indnt,
+                        const std::string_view dst_info) const -> void;
 
     [[nodiscard]] auto is_empty() const -> bool { return ops_.empty(); }
 
