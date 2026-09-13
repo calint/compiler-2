795235f11ce2a6722562254fd9c9824510ea5bb6
calint <calin.tenitchi@gmail.com>
2026-09-13 14:21:42 +0700

    .

--- src/stmt_builtin_array_copy.hpp
+++ src/stmt_builtin_array_copy.hpp
@@ -17,53 +17,58 @@
 #include "unary_ops.hpp"
 
 class stmt_builtin_array_copy final : public statement {
+    token open_paren_tk_;
     stmt_identifier from_;
+    token from_delim_tk_;
     stmt_identifier to_;
+    token to_delim_tk_;
     expr_any count_;
-    token ws1_; // whitespace after ')'
+    token close_paren_tk_;
+
   public:
     stmt_builtin_array_copy(toc& tc, token tk, tokenizer& tz) : statement{tk} {
 
         set_type(tc.get_type_void());
 
-        if (not tz.is_next_char('(')) {
+        open_paren_tk_ = tz.next_char_token('(');
+        if (open_paren_tk_.is_empty()) {
             throw compiler_exception{
                 tz, "expected '(' and 'from', 'to' and 'count'"};
         }
 
         from_ = {tc, {}, tz.next_token(), tz};
 
-        if (not tz.is_next_char(',')) {
+        from_delim_tk_ = tz.next_char_token(',');
+        if (from_delim_tk_.is_empty()) {
             throw compiler_exception{tz, "expected ',' then 'to' and 'count'"};
         }
 
         to_ = {tc, {}, tz.next_token(), tz};
 
-        if (not tz.is_next_char(',')) {
+        to_delim_tk_ = tz.next_char_token(',');
+        if (to_delim_tk_.is_empty()) {
             throw compiler_exception{tz, "expected ',' then 'count'"};
         }
 
         count_ = {tc, tz, tc.get_type_default(), true, false, 0};
 
-        if (not tz.is_next_char(')')) {
+        close_paren_tk_ = tz.next_char_token(')');
+        if (close_paren_tk_.is_empty()) {
             throw compiler_exception{tz, "expected ')' after the argument"};
         }
-
-        ws1_ = tz.next_whitespace_token();
     }
 
     stmt_builtin_array_copy() = default;
 
     auto source_to(std::ostream& os) const -> void override {
         statement::source_to(os);
-        std::print(os, "(");
+        open_paren_tk_.source_to(os);
         from_.source_to(os);
-        std::print(os, ",");
+        from_delim_tk_.source_to(os);
         to_.source_to(os);
-        std::print(os, ",");
+        to_delim_tk_.source_to(os);
         count_.source_to(os);
-        std::print(os, ")");
-        ws1_.source_to(os);
+        close_paren_tk_.source_to(os);
     }
 
     auto compile(toc& tc, std::ostream& os, const size_t indent,
