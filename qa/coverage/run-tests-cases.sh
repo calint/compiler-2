3768cd762988e54eab6ff019146779cacd6aaa90
calint <calin.tenitchi@gmail.com>
2026-09-14 22:21:37 +0700

    .

--- qa/coverage/run-tests-cases.sh
+++ qa/coverage/run-tests-cases.sh
@@ -366,5 +366,5 @@ SRC=t385 && COMPERR
 SRC=t386 && COMPERR
 SRC=t387 && COMPERR
 SRC=t388 && EXP=0 && RUN
-# SRC=t389 && COMPERR  # TODO: reject bool operands in arithmetic expressions
+# SRC=t389 && COMPERR  # todo: reject bool operands in arithmetic expressions
 SRC=t390 && COMPERR
