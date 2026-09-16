33dac6a9628e858d0dacebc4dbde2844957452ec
calint <calin.tenitchi@gmail.com>
2026-09-16 12:40:54 +0700

    .

--- src/stmt_builtin_foo.hpp
+++ src/stmt_builtin_foo.hpp
@@ -38,12 +38,27 @@ class stmt_builtin_foo final : public statement {
         }
 
         // static dry compilation
+
+        null_stream os;
+
         const ident_info ii{tc.make_ident_info(ident_)};
         tc.enter_foo("");
         const var_info e{
-            .name{"e"}, .type_ptr{&ii.type()}, .declared_at_tk{tok()}, .reg{}};
-        null_stream os;
+            .name{"e"},
+            .type_ptr{&ii.type()},
+            .declared_at_tk{},
+            .reg{},
+        };
         tc.add_var(tok(), os, 0, e, false);
+
+        const var_info var_i{
+            .name{"i"},
+            .type_ptr{&tc.get_type_default()},
+            .declared_at_tk{},
+            .reg{},
+        };
+        tc.add_var(tok(), os, 0, var_i, false);
+
         tc.add_const(tok(), os, 0, "n", static_cast<int64_t>(ii.array_size));
         code_ = {tc, tz};
         tc.exit_foo("");
@@ -70,17 +85,31 @@ class stmt_builtin_foo final : public statement {
 
         const std::string reg_iter{tc.alloc_scratch_register(
             tok(), os, indent, tc.get_type_default())};
-        const std::string reg_idx{tc.alloc_scratch_register(
-            tok(), os, indent, tc.get_type_default())};
 
         const ident_info ii{tc.make_ident_info(ident_)};
 
-        const var_info e{.name{"e"},
-                         .type_ptr{&ii.type()},
-                         .declared_at_tk{tok()},
-                         .reg{reg_iter}};
+        const var_info var_e{
+            .name{"e"},
+            .type_ptr{&ii.type()},
+            .declared_at_tk{tok()},
+            .reg{reg_iter},
+        };
 
-        tc.add_var(tok(), os, indent, e, false);
+        tc.add_var(tok(), os, indent, var_e, false);
+
+        const var_info var_i{
+            .name{"i"},
+            .type_ptr{&tc.get_type_default()},
+            .declared_at_tk{tok()},
+            .reg{},
+        };
+
+        tc.add_var(tok(), os, indent, var_i, false);
+
+        ident_info var_i_info{tc.make_ident_info(tok(), "i")};
+
+        const std::string& var_i_addr_op{
+            var_i_info.operand.str(toc::size_qword)};
 
         // load address of referenced array into 'reg_iter'
         if (ii.has_lea() or ident_.is_indexed()) {
@@ -104,17 +133,18 @@ class stmt_builtin_foo final : public statement {
         tc.add_const(tok(), os, indent, "n",
                      static_cast<int64_t>(ii.array_size));
 
-        toc::asm_xor(os, indent, reg_idx, reg_idx);
+        tc.asm_cmd(tok(), os, indent, "mov", var_i_addr_op, "0");
         toc::asm_label(os, indent, loop_label);
         code_.compile(tc, os, indent, toc::make_ident_info_empty());
-        toc::asm_label(os, indent, loop_label + "_continue");
-        toc::asm_add(os, indent, e.reg, std::format("{}", ii.type().size()));
-        toc::asm_add(os, indent, reg_idx, "1");
-        toc::asm_cmp(os, indent, reg_idx, std::format("{}", ii.array_size));
-        toc::asm_jne(os, indent, loop_label);
+        toc::asm_label(os, indent + 1, loop_label + "_continue");
+        toc::asm_add(os, indent + 2, reg_iter,
+                     std::format("{}", ii.type().size()));
+        toc::asm_add(os, indent + 2, var_i_addr_op, "1");
+        toc::asm_cmp(os, indent + 2, var_i_addr_op,
+                     std::format("{}", ii.array_size));
+        toc::asm_jne(os, indent + 2, loop_label);
         toc::asm_label(os, indent, loop_label + "_end");
 
-        tc.free_scratch_register(tok(), os, indent, reg_idx);
         tc.free_scratch_register(tok(), os, indent, reg_iter);
 
         tc.exit_foo(loop_label);
