b05453a249eb23b911e4fb91908dda1a6e2f8cb0
calint <calin.tenitchi@gmail.com>
2026-09-22 14:55:59 +0700

    .

--- qa/coverage/run-tests-all.sh
+++ qa/coverage/run-tests-all.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$(dirname "$0")"
 
-MACHINE=x86_64 bash "$SCRIPT_DIR/run-tests-coverage.sh" "$@"
-MACHINE=rv32i bash "$SCRIPT_DIR/run-tests-coverage.sh" "$@"
-bash "$SCRIPT_DIR/run-tests-cli.sh"
-bash "$SCRIPT_DIR/run-tests-arena.sh"
+MACHINE=x86_64 ./run-tests-coverage.sh
+MACHINE=rv32i SKIP_BUILD=1 ./run-tests-coverage.sh
+./run-tests-cli.sh
+./run-tests-arena.sh
