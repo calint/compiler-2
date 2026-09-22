31c71a4757d88a5ec4a06c4f2c0c8d69ec9e41b9
calint <calin.tenitchi@gmail.com>
2026-09-22 21:57:23 +0700

    .

--- run-baz.sh
+++ run-baz.sh
@@ -2,24 +2,63 @@
 set -e
 cd "$(dirname "$0")"
 
+TARGET=x86_64
+for arg in "$@"; do
+	case "$arg" in
+		--target=*) TARGET="${arg#--target=}" ;;
+	esac
+done
+case "$TARGET" in
+	x86_64|rv32i) ;;
+	*)
+		printf 'Unsupported target: %s (use --target=x86_64 or --target=rv32i)\n' "$TARGET" >&2
+		exit 1
+		;;
+esac
+
 SEP="--------------------------------------------------------------------------------"
 echo $SEP
-CMD="./baz $*"
-echo "$CMD"
-$CMD >gen.s
+printf './baz'
+printf ' %q' "$@"
+printf '\n'
+./baz "$@" >gen.s
 echo $SEP
-grep -v -e'^\s*;.*$' -e'^\s*$' gen.s >gen-without-comments.s
-grep -v -e'^\s*;.*$' -e'^\s*$' -e'^.*:\s*$' gen.s | wc | awk '{print "instructions: " $1}'
-grep -E '^\s*jmp\s.*.*$' gen.s | wc | awk '{print "jmp: " $1}'
-grep -E '^\s*j[a-z]{1,2}\s' gen.s | grep -v '^\s*jmp\s' | wc | awk '{print "jcc: " $1}'
+COMMENT=';'
+if [[ "$TARGET" == rv32i ]]; then
+	COMMENT='#'
+fi
+awk -v comment="$COMMENT" '$0 !~ "^[[:space:]]*" comment && $0 !~ /^[[:space:]]*$/ { print }' gen.s >gen-without-comments.s
+if [[ "$TARGET" == rv32i ]]; then
+	awk '
+		/^[[:space:]]*[[:alpha:]][[:alnum:]]*[[:space:]]/ {
+			instructions++
+			if ($1 == "j") jumps++
+			if ($1 ~ /^b/) branches++
+		}
+		END { print "instructions: " instructions+0; print "jmp: " jumps+0; print "jcc: " branches+0 }
+	' gen-without-comments.s
+else
+	grep -v -e'^\s*$' -e'^.*:\s*$' gen-without-comments.s | wc | awk '{print "instructions: " $1}'
+	grep -E '^\s*jmp\s.*.*$' gen.s | wc | awk '{print "jmp: " $1}'
+	grep -E '^\s*j[a-z]{1,2}\s' gen.s | grep -v '^\s*jmp\s' | wc | awk '{print "jcc: " $1}'
+fi
 echo $SEP
-nasm -f elf64 gen.s
-ld -s -T baz.ld -o gen gen.o
+if [[ "$TARGET" == rv32i ]]; then
+	llvm-mc -triple=riscv32 -mattr=-m,-a,-f,-d,-c -filetype=obj gen.s -o gen.o
+	ld.lld -m elf32lriscv -e _start -o gen gen.o
+else
+	nasm -f elf64 gen.s
+	ld -s -T baz.ld -o gen gen.o
+fi
 ls --color -la gen.s gen-without-comments.s gen
 echo $SEP
 
 set +e # don't stop at errors
-./gen
+if [[ "$TARGET" == rv32i ]]; then
+	qemu-riscv32 ./gen
+else
+	./gen
+fi
 RET=$?
 echo $SEP
 echo returned: $RET
