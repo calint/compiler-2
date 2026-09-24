8108ad9a9bc81d4b406b5569e678ebe445c235f0
calint <calin.tenitchi@gmail.com>
2026-09-24 08:02:30 +0700

    .

---
+++ qa/coverage/run-tests-coverage-full.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+cd "$(dirname "$0")"
+
+./run-tests-coverage.sh clean
+./run-tests-coverage.sh build
+./run-tests-coverage.sh --target=x86 run
+./run-tests-coverage.sh --target=rv32i run
+export LLVM_PROFILE_FILE="$PWD/extra-%p.profraw"
+./run-tests-cli.sh
+./run-tests-arena.sh
+./run-tests-coverage.sh report
